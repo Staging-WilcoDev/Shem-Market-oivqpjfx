cdbe951cfa98b486001e0ed062c1c056b6968e7f
Shem <smagnezi8@gmail.com>
2024-07-17 11:00:41 +0000

    test

--- node-server/src/server.js
+++ node-server/src/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 8001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
