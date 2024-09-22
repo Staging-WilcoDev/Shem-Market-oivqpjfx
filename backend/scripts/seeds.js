3b3418d5b77ca421c77547d830a75023df62eea0
WilcoApp <hello@wilco.gg>
2024-09-22 16:32:50 +0300

    Initial commit

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -6,45 +6,44 @@ require("../models/Comment");
 
 const User = mongoose.model("User");
 const Item = mongoose.model("Item");
+const Comment = mongoose.model("Comment");
 
-const connectToDatabase = () => {
-  const connection = process.env.MONGODB_URI || "mongodb://localhost:27017/anythink-market";
+const connectedToDatabase = () => {
+  const connection = process.env.MONGODB_URI || "mongodb://localhost:27017";
   mongoose.connect(connection);
   mongoose.set("debug", true);
 };
 
 async function main() {
-  connectToDatabase();
-  const verifiedUser = new User({
-    username: 'verifiedUser',
-    email: `verifiedUser@gmail.com`,
-    isVerified: true
-  });
-  await verifiedUser.save();
-
-  const notVerifiedUser = new User({
-      username: 'notVerifiedUser',
-      email: 'notVerifiedUser@gmail.com',
-      isVerified: false,
+  connectedToDatabase();
+  for (let i = 0; i < 100; i++) {
+    const user = {};
+    user.username = `user${i}`;
+    user.email = `user${i}@gmail.com`;
+    const options = { upsert: true, new: true };
+    const createdUser = await User.findOneAndUpdate(user, {}, options);
+    const item = {
+      slug: `slug${i}`,
+      title: `title ${i}`,
+      description: `description ${i}`,
+      seller: createdUser,
+    };
+    const createdItem = await Item.findOneAndUpdate(item, {}, options);
+    if (!createdItem?.comments?.length) {
+      let commentIds = [];
+      for (let j = 0; j < 100; j++) {
+        const comment = new Comment({
+          body: `body ${j}`,
+          seller: createdUser,
+          item: createdItem,
+        });
+        await comment.save();
+        commentIds.push(comment._id);
+      }
+      createdItem.comments = commentIds;
+      await createdItem.save();
     }
-  );
-  await notVerifiedUser.save();
-
-  const itemForVerifiedUser = new Item({
-    slug: `verified_seller_item`,
-    title: `Verified seller item`,
-    description: `description for the item`,
-    seller: verifiedUser,
-  });
-  await itemForVerifiedUser.save();
-
-  const itemForNotVerifiedUser = new Item({
-    slug: `not_verified_seller_item`,
-    title: `Not Verified seller item`,
-    description: `description for the item`,
-    seller: notVerifiedUser,
-  });
-  await itemForNotVerifiedUser.save();
+  }
 }
 
 main()
