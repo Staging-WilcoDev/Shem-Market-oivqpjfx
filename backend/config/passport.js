f668ec30b9894e6f31797ca3d2bc5f69eea5aacf
Shem <smagnezi8@gmail.com>
2024-10-15 18:05:48 +0300

    fix

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -3,34 +3,23 @@ var LocalStrategy = require("passport-local").Strategy;
 var mongoose = require("mongoose");
 var User = mongoose.model("User");
 
-function handleLogin(email, password) {
+function handleLogin(username, password) {
   return new Promise((resolve, reject) => {
-    User.findOne({ email: email })
-      .then(function (user) {
-        if (!user || !user.validPassword(password)) {
-          return reject({ errors: { "email or password": "is invalid" } });
-        }
-
-        return resolve(user);
-      })
-      .catch(reject);
+    User.findOne({ username: username, password: password }, (err, user) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(user);
+      }
+    });
   });
 }
 
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: "user[email]",
-      passwordField: "user[password]",
-    },
-    async function (email, password, done) {
-      handleLogin(email, password)
-        .then((user) => {
-          done(null, user);
-        })
-        .catch((error) => {
-          done(null, false, error);
-        });
-    }
-  )
-);
+// Call the function
+handleLogin("exampleUser", "examplePassword")
+  .then((user) => {
+    console.log("User found:", user);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
